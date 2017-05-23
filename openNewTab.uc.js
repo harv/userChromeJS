abdabcf9385a584cf2a8d5faa7c380ecbea75961
cinhoo <ch05042210@gmail.com>
2017-05-23 16:17:29 +0800

    support urlbar; add switch; fixed #5

--- openNewTab.uc.js
+++ openNewTab.uc.js
@@ -5,9 +5,14 @@
 // @include         chrome://browser/content/places/places.xul
 // @description     Open Bookmarks/History/Search in New Tab
 // @downloadURL     https://raw.githubusercontent.com/Harv/userChromeJS/master/openNewTab.uc.js
-// @version         1.2
+// @version         1.3
 // ==/UserScript==
 (function() {
+    var b_urlbar = false;
+    var b_searchbar = true;
+    var b_bookmarks = true;
+    var b_history = true;
+
     function whereToOpenLinkMod() {
         {
             if (!e) return 'current';
@@ -16,14 +21,18 @@
             var node = e.originalTarget;
             while (node) {
                 switch (node.id) {
-                    case 'bookmarksMenuPopup':  // menubar bookmarket
+                    case 'bookmarksMenuPopup':  // menubar bookmarks
+                    case 'BMB_bookmarksPopup':  // navibar bookmarks
+                    case 'bookmarksPanel':      // sidebar bookmarks
+                        return b_bookmarks ? 'tab' : 'current';
                     case 'goPopup':             // menubar history
-                    case 'BMB_bookmarksPopup':  // navibar bookmarket
                     case 'PanelUI-history':     // navibar history
-                    case 'bookmarksPanel':      // sidebar bookmarket
                     case 'history-panel':       // sidebar history
-                    case 'placeContent':        // library bookmarket&history
-                        return 'tab';
+                        return b_history ? 'tab' : 'current';
+                    case 'placeContent':        // library bookmarks&history
+                        var collection = window.document.getElementById('searchFilter').getAttribute('collection');
+                        var tab = collection === "bookmarks" && b_bookmarks || collection === "history" && b_history;
+                        return tab ? 'tab' : 'current';
                 }
                 node = node.parentNode;
             }
@@ -32,37 +41,33 @@
     }
     if (location == 'chrome://browser/content/browser.xul') {
         /* :::: Open Bookmarks/History in New Tab :::: */
-        eval('whereToOpenLink = ' + whereToOpenLink.toString().replace(/return "current";/g, whereToOpenLinkMod.toString().replace(/^.*{|}$/g, '')));
+        eval('whereToOpenLink = ' + whereToOpenLink.toString().replace(/return "current";/, whereToOpenLinkMod.toString().replace(/^.*{|}$/g, '')));
         window.document.getElementById('sidebar').addEventListener('DOMContentLoaded', function(event) {
             var doc = event.originalTarget;
             var win = doc.defaultView.window;
             if (win.location == 'chrome://browser/content/bookmarks/bookmarksPanel.xul' || win.location == 'chrome://browser/content/history/history-panel.xul') {
-                eval('win.whereToOpenLink=' + win.whereToOpenLink.toString().replace(/return "current";/g, whereToOpenLinkMod.toString().replace(/^.*{|}$/g, '')));
+                eval('win.whereToOpenLink=' + win.whereToOpenLink.toString().replace(/return "current";/, whereToOpenLinkMod.toString().replace(/^.*{|}$/g, '')));
             } else if (win.location == 'chrome://browser/content/readinglist/sidebar.xhtml') {
                 /* :::: Open Sidebar ReadingList in New Tab :::: */
-                eval('win.RLSidebar.openURL = ' + win.RLSidebar.openURL.toString().replace(/log\.debug\(.*\);/, '').replace(/mainWindow\.openUILink\(url, event\);/, (function(url, event) {
+                eval('win.RLSidebar.openURL = ' + win.RLSidebar.openURL.toString().replace(/log\.debug\(.*\);/, '').replace(/mainWindow\.openUILink\(url, event\);/, (function() {
                     var where = isTabEmpty(gBrowser.mCurrentTab) ? 'current' : 'tab';
-                }).toString().replace(/^.*{|}$/g, '') + '\nmainWindow.openUILinkIn(url, where);'));
+                }).toString().replace(/^.*{|}$/g, '') + '$&'));
             }
         });
+        /* :::: Open Url in New Tab :::: */
+        if (b_urlbar) {
+            var urlbar = document.getElementById('urlbar');
+            eval('urlbar.handleCommand=' + urlbar.handleCommand.toString().replace(/let where = openUILinkWhere;/, (function() {
+                let  where = isTabEmpty(gBrowser.mCurrentTab) ? 'current' : 'tab';
+            }).toString().replace(/^.*{|}$/g, '')));
+        }
         /* :::: Open Search in New Tab :::: */
-        var searchbar = document.getElementById('searchbar');
-        eval('searchbar.handleSearchCommand=' + searchbar.handleSearchCommand.toString().replace(/this\.doSearch\(textValue, where(, aEngine)?\);|this\.handleSearchCommandWhere\(aEvent, aEngine, where, params\);/, (function() {
-            {
-                if (isTabEmpty(gBrowser.mCurrentTab)) where = 'current';
-                else where = 'tab';
-                if (this.handleSearchCommandWhere) {
-                    // firefox 51+
-                    this.handleSearchCommandWhere(aEvent, aEngine, where, params);
-                } else {
-                    try { // firefox 34+
-                        this.doSearch(textValue, where, aEngine);
-                    } catch (e) {
-                        this.doSearch(textValue, where);
-                    }
-                }
-            }
-        }).toString().replace(/^.*{|}$/g, '')));
+        if (b_searchbar) {
+            var searchbar = document.getElementById('searchbar');
+            eval('searchbar.handleSearchCommand=' + searchbar.handleSearchCommand.toString().replace(/this\.doSearch\(textValue, where(, aEngine)?\);|this\.handleSearchCommandWhere\(aEvent, aEngine, where, params\);/, (function() {
+                where = isTabEmpty(gBrowser.mCurrentTab) ? 'current' : 'tab';
+            }).toString().replace(/^.*{|}$/g, '') + '$&'));
+        }
     } else if (location == 'chrome://browser/content/places/places.xul') {
         /* :::: Open Bookmarks/History in New Tab :::: */
         eval('whereToOpenLink = ' + whereToOpenLink.toString().replace(/return "current";/g, whereToOpenLinkMod.toString().replace(/^.*{|}$/g, '')));
