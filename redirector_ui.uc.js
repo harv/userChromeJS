94693bf975b4c17f1ecbabebd9c6b9d5d12698e4
cinhoo <me@haoutil.com>
2019-07-04 14:30:45 +0800

    remove nsIContentPolicy implementation

--- redirector_ui.uc.js
+++ redirector_ui.uc.js
@@ -8,19 +8,18 @@
 // @downloadURL     https://raw.githubusercontent.com/Harv/userChromeJS/master/redirector_ui.uc.js
 // @startup         Redirector.init();
 // @shutdown        Redirector.destroy(true);
-// @version         1.5.10
+// @version         1.6.0
 // ==/UserScript==
 location == "chrome://browser/content/browser.xul" && (function() {
     const {
-        Cc: classes,
-        Ci: interfaces,
-        Cu: utils,
-        Cr: results,
+        classes: Cc,
+        interfaces: Ci,
+        utils: Cu,
+        results: Cr,
     } = Components;
 
     Cu.import("resource://gre/modules/XPCOMUtils.jsm");
     Cu.import("resource://gre/modules/Services.jsm");
-    Cu.import("resource://gre/modules/NetUtil.jsm");
 
     function RedirectorUI() {
         this.rules = "local/_redirector.js".split("/"); // 规则文件路径
@@ -37,11 +36,7 @@ location == "chrome://browser/content/browser.xul" && (function() {
                 XPCOMUtils.defineLazyGetter(Services, "redirector", function() {
                     let redirector = new Redirector();
                     redirector.clearCache = function() {
-                        // clear cache
-                        this._cache = {
-                            redirectUrl: {},
-                            clickUrl: {}
-                        };
+                        this.redirectUrls = {};
                     };
                     redirector.rules = [];
                     redirector.state = state;
@@ -54,7 +49,7 @@ location == "chrome://browser/content/browser.xul" && (function() {
         init: function() {
             this.state = this.redirector.state;
             if (this.state) {
-                this.redirector.init(window);
+                this.redirector.init();
             }
             this.loadRule();
             this.drawUI();
@@ -64,7 +59,7 @@ location == "chrome://browser/content/browser.xul" && (function() {
             Services.cpmm.addMessageListener("redirector:reload", this);
         },
         destroy: function(shouldDestoryUI) {
-            this.redirector.destroy(window);
+            this.redirector.destroy();
             if (shouldDestoryUI) {
                 this.destoryUI();
             }
@@ -303,42 +298,19 @@ location == "chrome://browser/content/browser.xul" && (function() {
     };
     function Redirector() {
         this.rules = [];
-        this._cache = {
-            redirectUrl: {},
-            clickUrl: {}
-        };
+        this.redirectUrls = {};
     }
     Redirector.prototype = {
-        classDescription: "Redirector content policy",
-        classID: Components.ID("{1d5903f0-6b5b-4229-8673-76b4048c6675}"),
-        contractID: "@haoutil.com/redirector/policy;1",
-        xpcom_categories: ["content-policy", "net-channel-event-sinks"],
-        init: function(window) {
-            window.addEventListener("click", this, false);
-            let registrar = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
-            if (!registrar.isCIDRegistered(this.classID)) {
-                registrar.registerFactory(this.classID, this.classDescription, this.contractID, this);
-                let catMan = Cc["@mozilla.org/categorymanager;1"].getService(Ci.nsICategoryManager);
-                for (let category of this.xpcom_categories)
-                    catMan.addCategoryEntry(category, this.contractID, this.contractID, false, true);
-                Services.obs.addObserver(this, "http-on-modify-request", false);
-                Services.obs.addObserver(this, "http-on-examine-response", false);
-            }
+        init: function() {
+            Services.obs.addObserver(this, "http-on-modify-request", false);
+            Services.obs.addObserver(this, "http-on-examine-response", false);
         },
-        destroy: function(window) {
-            window.removeEventListener("click", this, false);
-            let registrar = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
-            if (registrar.isCIDRegistered(this.classID)) {
-                registrar.unregisterFactory(this.classID, this);
-                let catMan = Cc["@mozilla.org/categorymanager;1"].getService(Ci.nsICategoryManager);
-                for (let category of this.xpcom_categories)
-                    catMan.deleteCategoryEntry(category, this.contractID, false);
-                Services.obs.removeObserver(this, "http-on-modify-request", false);
-                Services.obs.removeObserver(this, "http-on-examine-response", false);
-            }
+        destroy: function() {
+            Services.obs.removeObserver(this, "http-on-modify-request", false);
+            Services.obs.removeObserver(this, "http-on-examine-response", false);
         },
         getRedirectUrl: function(originUrl) {
-            let redirectUrl = this._cache.redirectUrl[originUrl];
+            let redirectUrl = this.redirectUrls[originUrl];
             if(typeof redirectUrl !== "undefined") {
                 return redirectUrl;
             }
@@ -358,7 +330,7 @@ location == "chrome://browser/content/browser.xul" && (function() {
                     }
                     rule.computed = {regex: regex, from: from, to: to, exclude: exclude, decode: decode};
                 }
-                url = decode ? this.decodeUrl(originUrl) : originUrl;
+                url = decode ? decodeURIComponent(originUrl) : originUrl;
                 redirect = regex
                     ? from.test(url) ? !(exclude && exclude.test(url)) : false
                     : from == url ? !(exclude && exclude == url) : false;
@@ -367,115 +339,43 @@ location == "chrome://browser/content/browser.xul" && (function() {
                         ? regex ? to(url.match(from)) : to(from)
                         : regex ? url.replace(from, to) : to;
                     redirectUrl = {
-                        url : decode ? url : this.decodeUrl(url),    // 避免二次解码
+                        url : decode ? url : decodeURIComponent(url),    // 避免二次解码
                         resp: rule.resp
                     };
                     break;
                 }
             }
-            this._cache.redirectUrl[originUrl] = redirectUrl;
+            this.redirectUrls[originUrl] = redirectUrl;
             return redirectUrl;
         },
-        decodeUrl: function(encodedUrl) {
-            let decodedUrl;
-            try {
-                decodedUrl = decodeURIComponent(encodedUrl);
-            } catch(e) {
-                decodedUrl = encodedUrl;
-            }
-            return decodedUrl;
-        },
         wildcardToRegex: function(wildcard) {
-            if (!wildcard)
-                return null;
             return new RegExp((wildcard + "").replace(new RegExp("[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\-]", "g"), "\\$&").replace(/\\\*/g, "(.*)").replace(/\\\?/g, "."), "i");
         },
-        getTarget: function(redirectUrl, callback) {
-            NetUtil.asyncFetch(redirectUrl.url, function(inputStream, status) {
-                let binaryOutputStream = Cc['@mozilla.org/binaryoutputstream;1'].createInstance(Ci['nsIBinaryOutputStream']);
-                let storageStream = Cc['@mozilla.org/storagestream;1'].createInstance(Ci['nsIStorageStream']);
-                let count = inputStream.available();
-                let data = NetUtil.readInputStreamToString(inputStream, count);
-                storageStream.init(512, count, null);
-                binaryOutputStream.setOutputStream(storageStream.getOutputStream(0));
-                binaryOutputStream.writeBytes(data, count);
-                redirectUrl.storageStream = storageStream;
-                redirectUrl.count = count;
-                if (typeof callback === 'function')
-                    callback();
+        doRedirect: function(http, redirectUrl, replaceResponse) {
+            http.suspend();
+            http.QueryInterface(Ci.nsITraceableChannel);
+            let oldListener = http.setNewListener({
+                onStartRequest: function() {},
+                onStopRequest: function() {},
+                onDataAvailable: function() {},
             });
-        },
-        // nsIDOMEventListener interface implementation
-        handleEvent: function(event) {
-            if (!event.ctrlKey && "click" === event.type && 1 === event.which) {
-                let target = event.target;
-                if (!target.href || typeof this._cache.clickUrl[target.href] !== "undefined") return;
-                this._cache.clickUrl[target.href] = false;
-                while(target) {
-                    if (target.tagName && "BODY" === target.tagName.toUpperCase()) break;
-                    if (target.tagName && "A" === target.tagName.toUpperCase()
-                        && target.target && "_BLANK" === target.target.toUpperCase()
-                        && target.href) {
-                        this._cache.clickUrl[target.href] = true;
-                        break;
-                    }
-                    target = target.parentNode;
-                }
-            }
-        },
-        // nsIContentPolicy interface implementation
-        shouldLoad: function(contentType, contentLocation, requestOrigin, context, mimeTypeGuess, extra) {
-            // don't redirect clicking links with "_blank" target attribute
-            // cause links will be loaded in current tab/window
-            if (this._cache.clickUrl[contentLocation.spec]) {
-                return Ci.nsIContentPolicy.ACCEPT;
-            }
-            // only redirect documents
-            if (contentType != Ci.nsIContentPolicy.TYPE_DOCUMENT)
-                return Ci.nsIContentPolicy.ACCEPT;
-            if (!context || !context.loadURI)
-                return Ci.nsIContentPolicy.ACCEPT;
-            let redirectUrl = this.getRedirectUrl(contentLocation.spec);
-            if (redirectUrl && !redirectUrl.resp) {
-                context.loadURI(redirectUrl.url, requestOrigin, null);
-                return Ci.nsIContentPolicy.REJECT_REQUEST;
-            }
-            return Ci.nsIContentPolicy.ACCEPT;
-        },
-        shouldProcess: function(contentType, contentLocation, requestOrigin, context, mimeTypeGuess, extra) {
-            return Ci.nsIContentPolicy.ACCEPT;
-        },
-        // nsIChannelEventSink interface implementation
-        asyncOnChannelRedirect: function(oldChannel, newChannel, flags, redirectCallback) {
-            this.onChannelRedirect(oldChannel, newChannel, flags);
-            redirectCallback.onRedirectVerifyCallback(Cr.NS_OK);
-        },
-        onChannelRedirect: function(oldChannel, newChannel, flags) {
-            // only redirect documents
-            if (!(newChannel.loadFlags & Ci.nsIChannel.LOAD_DOCUMENT_URI))
-                return;
-            let newLocation = newChannel.URI.spec;
-            if (!newLocation)
-                return;
-            let callbacks = [];
-            if (newChannel.notificationCallbacks)
-                callbacks.push(newChannel.notificationCallbacks);
-            if (newChannel.loadGroup && newChannel.loadGroup.notificationCallbacks)
-                callbacks.push(newChannel.loadGroup.notificationCallbacks);
-            let win, webNav;
-            for (let callback of callbacks) {
-                try {
-                    win = callback.getInterface(Ci.nsILoadContext).associatedWindow;
-                    webNav = win.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIWebNavigation);
-                    break;
-                } catch(e) {}
-            }
-            if (!webNav)
-                return;
-            let redirectUrl = this.getRedirectUrl(newLocation);
-            if (redirectUrl && !redirectUrl.resp) {
-                webNav.loadURI(redirectUrl.url, null, null, null, null);
-            }
+            let loadInfo = http.loadInfo;
+            let uri = Services.io.newURI(redirectUrl.url, null, null);
+            let newHttp = Services.io.newChannelFromURIWithLoadInfo(uri, loadInfo);
+            if (replaceResponse) newHttp.originalURI = http.URI;
+            newHttp.asyncOpen({
+                onStartRequest: function(request, context) {
+                    oldListener.onStartRequest(request, context);
+                },
+                onStopRequest: function(request, context, statusCode) {
+                    oldListener.onStopRequest(request, context, statusCode);
+                    http.resume();
+                    http.cancel(Cr.NS_BINDING_REDIRECTED);
+                },
+                onDataAvailable: function(request, context, inputStream, offset, count) {
+                    oldListener.onDataAvailable(request, context, inputStream, offset, count);
+                },
+            }, null);
         },
         // nsIObserver interface implementation
         observe: function(subject, topic, data, additional) {
@@ -483,29 +383,22 @@ location == "chrome://browser/content/browser.xul" && (function() {
                 case "http-on-modify-request": {
                     let http = subject.QueryInterface(Ci.nsIHttpChannel);
                     let redirectUrl = this.getRedirectUrl(http.URI.spec);
-                    if (redirectUrl && !redirectUrl.resp)
-                        if(http.redirectTo)
-                            // firefox 20+
-                            http.redirectTo(Services.io.newURI(redirectUrl.url, null, null));
-                        else
-                            // others replace response body
-                            redirectUrl.resp = true;
+                    if (redirectUrl && !redirectUrl.resp) {
+                        // this.doRedirect(http, redirectUrl, false);
+                        http.cancel(Cr.NS_BINDING_REDIRECTED); // NS_BINDING_ABORTED
+                        let loadInfo = http.loadInfo;
+                        let uri = Services.io.newURI(redirectUrl.url, null, null);
+                        loadInfo.loadingContext.webNavigation.loadURI(redirectUrl.url, {
+                            triggeringPrincipal: Services.scriptSecurityManager.createCodebasePrincipal(uri, loadInfo.triggeringPrincipal),
+                        });
+                    }
                     break;
                 }
                 case "http-on-examine-response": {
                     let http = subject.QueryInterface(Ci.nsIHttpChannel);
                     let redirectUrl = this.getRedirectUrl(http.URI.spec);
                     if (redirectUrl && redirectUrl.resp) {
-                        if (!redirectUrl.storageStream || !redirectUrl.count) {
-                            http.suspend();
-                            this.getTarget(redirectUrl, function() {
-                                http.resume();
-                            });
-                        }
-                        let newListener = new TrackingListener();
-                        subject.QueryInterface(Ci.nsITraceableChannel);
-                        newListener.originalListener = subject.setNewListener(newListener);
-                        newListener.redirectUrl = redirectUrl;
+                        this.doRedirect(http, redirectUrl, true);
                     }
                     break;
                 }
@@ -518,25 +411,7 @@ location == "chrome://browser/content/browser.xul" && (function() {
             return this.QueryInterface(iid);
         },
         // nsISupports interface implementation
-        QueryInterface: (ChromeUtils.generateQI || XPCOMUtils.generateQI)([Ci.nsIContentPolicy, Ci.nsIChannelEventSink, Ci.nsIObserver, Ci.nsIFactory, Ci.nsISupports])
-    };
-    function TrackingListener() {
-        this.originalListener = null;
-        this.redirectUrl = null;
-    }
-    TrackingListener.prototype = {
-        // nsITraceableChannel interface implementation
-        onStartRequest: function(request, context) {
-            this.originalListener.onStartRequest(request, context);
-        },
-        onStopRequest: function(request, context) {
-            this.originalListener.onStopRequest(request, context, Cr.NS_OK);
-        },
-        onDataAvailable: function(request, context) {
-            this.originalListener.onDataAvailable(request, context, this.redirectUrl.storageStream.newInputStream(0), 0, this.redirectUrl.count);
-        },
-        // nsISupports interface implementation
-        QueryInterface: (ChromeUtils.generateQI || XPCOMUtils.generateQI)([Ci.nsIStreamListener, Ci.nsISupports])
+        QueryInterface: (ChromeUtils.generateQI || XPCOMUtils.generateQI)([Ci.nsIObserver, Ci.nsIFactory, Ci.nsISupports])
     };
 
     if (window.Redirector) {
